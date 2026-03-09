2a1637c968386268807f1cbabe11fd4eda49be7d
Sabrina Dignard <sabrina.di97@hotmail.fr>
2026-03-09 12:09:19 -0400

    Avancement js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-const buttons = document.querySelectorAll('button');
+//Variables
+
+const buttons = document.querySelectorAll('.button');
+const modal = document.querySelector('.modal');
+
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+
+    button.addEventListener('click', openModal);
+}
+
+function openModal(){
+    const texte = document.createElement('p');
+    texte.innerText = 'modale';
+    texte.classList.add('fenetre');
+
+    modal.appendChild(texte);
+    modal.classList.add('visible');
+
+    texte.addEventListener('click', closeModal);
+}
+
+function closeModal(event){
+    modal.removeChild(event.currentTarget);
+    modal.classList.remove('visible');
+}
